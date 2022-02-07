477200849e004a10c6f94d4949c62f1147dbd4c1
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-02-07 10:53:10 -0800

    intial setup

--- main.py
+++ main.py
@@ -19,9 +19,12 @@
 #
 # # Keyword Method with iterrows()
 # # {new_key:new_value for (index, row) in df.iterrows()}
+
 import pandas
 #TODO 1. Create a dictionary in this format:
 # {"A": "Alfa", "B": "Bravo"}
+
+print("Hello World")
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
